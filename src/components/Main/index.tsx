354a6a1438e3b781a3bd9b94dc1eb409970877a5
jananikumar24 <jananik967@gmail.com>
2024-11-26 14:37:45 +0530

    Add info 🐣

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="w-full flex-col items-center justify-between p-12">
       <h1 className="text-9xl font-bold">Publication Summary Tool</h1>
+      <p> This tool is going to do something cool for Smart India Hackathon</p>
     </main>
   );
 }
