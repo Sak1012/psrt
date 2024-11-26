4145a457ecda09d1aba4131c317e48382aced940
Sak1012 <1012srivatsavas@gmail.com>
2024-11-26 14:01:10 +0530

    init 🎀

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="w-full flex-col items-center justify-between p-12">
+      <h1 className="text-9xl font-bold">Publication Summary Tool</h1>
+    </main>
+  );
+}
